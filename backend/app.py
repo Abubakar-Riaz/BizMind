e5699428607f14f1e9d69352ba9145c16d50a28d
Abubakar-Riaz <abubakarriaz416@gmail.com>
2026-01-17 00:42:01 +0500

    feat: added GET and POST API routes for products

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
@@ -15,6 +15,25 @@ class Product(db.Model):
     name=db.Column(db.String(100),nullable=False)
     price=db.Column(db.Float,nullable=False)
     stock=db.Column(db.Integer,default=0)
+
+@app.route('/products',methods=['GET'])
+def get_products():
+    products=Product.query.all()
+
+    output=[]
+    for product in products:
+        product_data={'id':product.id, 'name':product.name, 'price':product.price, 'stock':product.stock}
+        output.append(product_data)
+    return jsonify({"product":output})
+
+@app.route('/products',methods=['POST'])
+def add_product():
+    data=request.get_json()
+
+    new_product=Product(name=data['name'],price=data['price'],stock=data.get('stock',0))
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({'message':'Product Created Successfully'})
 
 if __name__=='__main__':
     with app.app_context():
